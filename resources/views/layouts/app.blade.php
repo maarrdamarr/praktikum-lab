df9819a42bde74777540bf97034ea59a34c0ca61
maarrdamarr <damardarma42@gmail.com>
2026-05-20 10:42:28 +0700

    main

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -122,6 +122,7 @@
                 @endphp
 
                 <div class="space-y-1">
+                    @if($role == 'praktikan')
                         <x-nav-link route="praktikan.dashboard" icon="m3 12 2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" label="Dashboard" />
                         <x-nav-link route="absensi" icon="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" label="Absensi Kehadiran" />
                         <x-nav-link route="praktikan.pendaftaran" icon="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" label="Pendaftaran" />
